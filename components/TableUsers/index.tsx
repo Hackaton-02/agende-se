7595cd99846fad0c091374875b8f0ba8f300e3a3
João Otávio <joaotavioos@outlook.com>
2022-04-10 13:13:17 -0300

    feat: adding TableUsers component ✨

---
+++ components/TableUsers/index.tsx
@@ -0,0 +1,313 @@
+import { Table } from 'react-bootstrap'
+import * as S from "./styles"
+
+const TableUsers: React.FC = () => {
+  return (
+    <S.TableWrapper>
+    <Table responsive>
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Perfil</th>
+          <th>Email</th>
+          <th></th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>Ana Silva</td>
+          <td>Admin</td>
+          <td>anasilva@gmail.com</td>
+          <td>
+            <i
+              className="fa fa-edit"
+              style={{ color: 'grey', cursor: 'pointer', marginRight: "30px" }}
+              onClick={() => {}}
+            />
+            <i
+              className="fa fa-trash"
+              style={{ color: 'red', cursor: 'pointer' }}
+              onClick={() => {}}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Ana Silva</td>
+          <td>Admin</td>
+          <td>anasilva@gmail.com</td>
+          <td>
+            <i
+              className="fa fa-edit"
+              style={{ color: 'grey', cursor: 'pointer', marginRight: "30px" }}
+              onClick={() => {}}
+            />
+            <i
+              className="fa fa-trash"
+              style={{ color: 'red', cursor: 'pointer' }}
+              onClick={() => {}}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Ana Silva</td>
+          <td>Admin</td>
+          <td>anasilva@gmail.com</td>
+          <td>
+            <i
+              className="fa fa-edit"
+              style={{ color: 'grey', cursor: 'pointer', marginRight: "30px" }}
+              onClick={() => {}}
+            />
+            <i
+              className="fa fa-trash"
+              style={{ color: 'red', cursor: 'pointer' }}
+              onClick={() => {}}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Ana Silva</td>
+          <td>Admin</td>
+          <td>anasilva@gmail.com</td>
+          <td>
+            <i
+              className="fa fa-edit"
+              style={{ color: 'grey', cursor: 'pointer', marginRight: "30px" }}
+              onClick={() => {}}
+            />
+            <i
+              className="fa fa-trash"
+              style={{ color: 'red', cursor: 'pointer' }}
+              onClick={() => {}}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Ana Silva</td>
+          <td>Admin</td>
+          <td>anasilva@gmail.com</td>
+          <td>
+            <i
+              className="fa fa-edit"
+              style={{ color: 'grey', cursor: 'pointer', marginRight: "30px" }}
+              onClick={() => {}}
+            />
+            <i
+              className="fa fa-trash"
+              style={{ color: 'red', cursor: 'pointer' }}
+              onClick={() => {}}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Ana Silva</td>
+          <td>Admin</td>
+          <td>anasilva@gmail.com</td>
+          <td>
+            <i
+              className="fa fa-edit"
+              style={{ color: 'grey', cursor: 'pointer', marginRight: "30px" }}
+              onClick={() => {}}
+            />
+            <i
+              className="fa fa-trash"
+              style={{ color: 'red', cursor: 'pointer' }}
+              onClick={() => {}}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Ana Silva</td>
+          <td>Admin</td>
+          <td>anasilva@gmail.com</td>
+          <td>
+            <i
+              className="fa fa-edit"
+              style={{ color: 'grey', cursor: 'pointer', marginRight: "30px" }}
+              onClick={() => {}}
+            />
+            <i
+              className="fa fa-trash"
+              style={{ color: 'red', cursor: 'pointer' }}
+              onClick={() => {}}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Ana Silva</td>
+          <td>Admin</td>
+          <td>anasilva@gmail.com</td>
+          <td>
+            <i
+              className="fa fa-edit"
+              style={{ color: 'grey', cursor: 'pointer', marginRight: "30px" }}
+              onClick={() => {}}
+            />
+            <i
+              className="fa fa-trash"
+              style={{ color: 'red', cursor: 'pointer' }}
+              onClick={() => {}}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Ana Silva</td>
+          <td>Admin</td>
+          <td>anasilva@gmail.com</td>
+          <td>
+            <i
+              className="fa fa-edit"
+              style={{ color: 'grey', cursor: 'pointer', marginRight: "30px" }}
+              onClick={() => {}}
+            />
+            <i
+              className="fa fa-trash"
+              style={{ color: 'red', cursor: 'pointer' }}
+              onClick={() => {}}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Ana Silva</td>
+          <td>Admin</td>
+          <td>anasilva@gmail.com</td>
+          <td>
+            <i
+              className="fa fa-edit"
+              style={{ color: 'grey', cursor: 'pointer', marginRight: "30px" }}
+              onClick={() => {}}
+            />
+            <i
+              className="fa fa-trash"
+              style={{ color: 'red', cursor: 'pointer' }}
+              onClick={() => {}}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Ana Silva</td>
+          <td>Admin</td>
+          <td>anasilva@gmail.com</td>
+          <td>
+            <i
+              className="fa fa-edit"
+              style={{ color: 'grey', cursor: 'pointer', marginRight: "30px" }}
+              onClick={() => {}}
+            />
+            <i
+              className="fa fa-trash"
+              style={{ color: 'red', cursor: 'pointer' }}
+              onClick={() => {}}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Ana Silva</td>
+          <td>Admin</td>
+          <td>anasilva@gmail.com</td>
+          <td>
+            <i
+              className="fa fa-edit"
+              style={{ color: 'grey', cursor: 'pointer', marginRight: "30px" }}
+              onClick={() => {}}
+            />
+            <i
+              className="fa fa-trash"
+              style={{ color: 'red', cursor: 'pointer' }}
+              onClick={() => {}}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Ana Silva</td>
+          <td>Admin</td>
+          <td>anasilva@gmail.com</td>
+          <td>
+            <i
+              className="fa fa-edit"
+              style={{ color: 'grey', cursor: 'pointer', marginRight: "30px" }}
+              onClick={() => {}}
+            />
+            <i
+              className="fa fa-trash"
+              style={{ color: 'red', cursor: 'pointer' }}
+              onClick={() => {}}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Ana Silva</td>
+          <td>Admin</td>
+          <td>anasilva@gmail.com</td>
+          <td>
+            <i
+              className="fa fa-edit"
+              style={{ color: 'grey', cursor: 'pointer', marginRight: "30px" }}
+              onClick={() => {}}
+            />
+            <i
+              className="fa fa-trash"
+              style={{ color: 'red', cursor: 'pointer' }}
+              onClick={() => {}}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Ana Silva</td>
+          <td>Admin</td>
+          <td>anasilva@gmail.com</td>
+          <td>
+            <i
+              className="fa fa-edit"
+              style={{ color: 'grey', cursor: 'pointer', marginRight: "30px" }}
+              onClick={() => {}}
+            />
+            <i
+              className="fa fa-trash"
+              style={{ color: 'red', cursor: 'pointer' }}
+              onClick={() => {}}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Ana Silva</td>
+          <td>Admin</td>
+          <td>anasilva@gmail.com</td>
+          <td>
+            <i
+              className="fa fa-edit"
+              style={{ color: 'grey', cursor: 'pointer', marginRight: "30px" }}
+              onClick={() => {}}
+            />
+            <i
+              className="fa fa-trash"
+              style={{ color: 'red', cursor: 'pointer' }}
+              onClick={() => {}}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Ana Silva</td>
+          <td>Admin</td>
+          <td>anasilva@gmail.com</td>
+          <td>
+            <i
+              className="fa fa-edit"
+              style={{ color: 'grey', cursor: 'pointer', marginRight: "30px" }}
+              onClick={() => {}}
+            />
+            <i
+              className="fa fa-trash"
+              style={{ color: 'red', cursor: 'pointer' }}
+              onClick={() => {}}
+            />
+          </td>
+        </tr>
+      </tbody>
+    </Table>
+    </S.TableWrapper>
+  )
+}
+
+export default TableUsers
