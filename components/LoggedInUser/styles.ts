0fe888c475cab9e0f0aa9915db538d1c6c748f53
João Otávio <joaotavioos@outlook.com>
2022-04-10 19:10:35 -0300

    style: add navbar animation 💎

--- components/LoggedInUser/styles.ts
+++ components/LoggedInUser/styles.ts
@@ -20,8 +20,10 @@ export const UserNameText = styled.p`
     cursor: pointer;
   }
 
-  img {
+  img,
+  svg {
     cursor: pointer;
+    animation: loader 1.2s linear forwards;
   }
 
   .logout {
@@ -32,6 +34,7 @@ export const UserNameText = styled.p`
     //    color: #000000;
     color: white;
     margin-right: 50px;
+    animation: opacity 1s linear forwards;
   }
 
   .avatar {
@@ -40,4 +43,76 @@ export const UserNameText = styled.p`
   ${media.lessThan('medium')`
     margin-right: 15vw;
   `}
+
+
+  @keyframes opacity {
+    0% {
+      opacity: 0;
+      display: none;
+    }
+
+    50% {
+      display: none;
+
+    }
+
+    85% {
+      display: flex;
+      opacity: 0.2;
+    }
+
+    100% {
+      opacity: 1;
+      display: flex;
+
+    }
+  }
+
+  @keyframes loader {
+    0% {
+      rotate: 0deg;
+      opacity: 0;
+    }
+    25% {
+      rotate: z 190deg;
+      transform: translateX(-70px);
+      opacity: 0.05;
+    }
+
+    75% {
+      rotate: z 270deg;
+      opacity: 0.2;
+    }
+    100% {
+      rotate: 360deg;
+      transform: translateX(0px);
+      opacity: 1;
+
+    }
+  }
+
+  @supports not (rotate: 0deg) {
+    @keyframes loader {
+      0% {
+        transform: rotate(0deg);
+        opacity: 0;
+
+      }
+      25% {
+        transform: translateX(-70px) rotateZ(190deg);
+        opacity: 0.05;
+      }
+
+      75% {
+        transform: rotateZ(270deg);
+        opacity: 0.2;
+
+      }
+      100% {
+        transform: translateX(0px) rotate(360deg);
+        opacity: 1;
+
+      }
+    }
+  }
 `
