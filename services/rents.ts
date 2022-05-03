79550fa7d541b205d5e1e8bbd38fcc210c3220fc
João Otávio <joaotavioos@outlook.com>
2022-05-03 12:27:05 -0300

    feat: add room rent service ✨

---
+++ services/rents.ts
@@ -0,0 +1,20 @@
+import api from './api';
+import RoomRent from '../dtos/RoomRent';
+import Meta from 'dtos/Meta';
+
+interface RoomRentsIndexData {
+  rents: RoomRent[]
+  meta: Meta
+}
+
+const RentService = {
+  index(url: string = "/storefront/v1/room_rents") {
+    return api.get<RoomRentsIndexData>(url).then(resp => resp.data);
+  },
+
+  show(id: number) {
+    return api.get<RoomRent>(`/storefront/v1/room_rents/${id}`).then(resp => resp.data);
+  }
+}
+
+export default RentService;
