a0b2bf6da6156e6348e375d58991e2376900d5c8
João Otávio <joaotavioos@outlook.com>
2022-05-03 12:28:55 -0300

    feat: add booking validation service ✨

---
+++ services/booking_validation.ts
@@ -0,0 +1,14 @@
+import api from './api';
+
+
+interface BookingIndexData {
+  range: [Date]
+}
+
+const BookingValidationService = {
+  index(url: string) {
+    return api.get<BookingIndexData>(url).then(resp => resp.data);
+  }
+}
+
+export default BookingValidationService;
