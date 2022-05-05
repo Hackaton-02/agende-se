c7351d7acca12e0d7c08b2ee5326d42e1bb61c69
João Otávio <joaotavioos@outlook.com>
2022-05-05 08:39:04 -0300

    feat: add NoData component ✨

---
+++ components/shared/NoData/index.tsx
@@ -0,0 +1,15 @@
+import * as S from './styles';
+
+
+interface NoDataProps {
+  message?: string
+}
+const NoData: React.FC<NoDataProps> =
+  ({ message = 'Não há dados cadastrados ou encontrados =(' }) => {
+  return (
+    <S.Painel>
+      { message }
+    </S.Painel>
+  )
+}
+export default NoData;
