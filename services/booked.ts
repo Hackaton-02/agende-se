2af307d4777beb69ad1b06b780eab2471364b1cb
João Otávio <joaotavioos@outlook.com>
2022-05-06 08:55:53 -0300

    feat: add booking patch services ✨

--- services/booked.ts
+++ services/booked.ts
@@ -1,14 +1,24 @@
-import Books from 'dtos/Books';
-import api from './api';
+import Books from 'dtos/Books'
+import api from './api'
 
 interface BookedIndexData {
   consultsBooked: [Books]
+  rentalOrders: [Books]
+  consultOrders: [Books]
 }
 
+type BookUpdate = Pick<Books, 'id' | 'accepted'>
+
 const BookedService = {
-  index(url: string = "/storefront/v1/booked") {
-    return api.get<BookedIndexData>(url).then(resp => resp.data);
+  index(url: string = '/storefront/v1/booked') {
+    return api.get<BookedIndexData>(url).then(resp => resp.data)
+  },
+  update_especialist: ({ id, ...book }: BookUpdate) => {
+    return api.patch<void>(`/especialista/v1/booked/${id}`, { book })
+  },
+  update_admin: ({ id, ...book }: BookUpdate) => {
+    return api.patch<void>(`/admin/v1/booked/${id}`, { book })
   }
 }
 
-export default BookedService;
+export default BookedService
