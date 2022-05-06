9f8925d4137732cde0337eb67819e50a3e6d7f9c
João Otávio <joaotavioos@outlook.com>
2022-05-06 08:55:19 -0300

    refactor: add loader and refactor data types 📦

--- pages/Bookmarked/index.tsx
+++ pages/Bookmarked/index.tsx
@@ -3,29 +3,46 @@ import Loader from 'components/Loader'
 import Menu from 'components/Menu'
 import MainComponent from 'components/shared/MainComponent'
 import NoData from 'components/shared/NoData'
+import AuthState from 'dtos/AuthState'
 import { NextPage } from 'next'
+import { useSelector } from 'react-redux'
 import { toast } from 'react-toastify'
 import BookedService from 'services/booked'
 import useSWR from 'swr'
 
 const Bookmarked: NextPage = () => {
-  const { data, error } = useSWR(`/storefront/v1/booked`, BookedService.index)
+  const { profile } = useSelector((state: AuthState) => state.auth.loggedUser)
+
+  const namespace =
+    profile === 'admin'
+      ? 'admin'
+      : profile === 'especialista'
+      ? 'especialista'
+      : 'storefront'
+
+  const { data, error } = useSWR(`/${namespace}/v1/booked`, BookedService.index)
+
+  const params =
+    profile === 'admin'
+      ? data?.rentalOrders
+      : profile === 'especialista'
+      ? data?.consultOrders
+      : data?.consultsBooked
 
   if (error) {
-    toast.error('Erro ao obter dados dos consultórios!')
+    toast.error('Erro ao obter dados das consultas ou alugueis!')
     console.log(error)
   }
-  console.log(data)
 
   return (
     <MainComponent>
       <Menu />
       {!data ? (
         <Loader />
-      ) : data.consultsBooked.length < 1 ? (
-        <NoData message="não há consultas cadastradas" />
+      ) : params?.length! < 1 ? (
+        <NoData message="Não há consultas cadastradas" />
       ) : (
-        <BookedContainer books={data.consultsBooked} />
+        <BookedContainer books={params!} />
       )}
     </MainComponent>
   )
