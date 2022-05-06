8f77d5f61f1c1b0b98ae17b43888ef8c1da912a9
João Otávio <joaotavioos@outlook.com>
2022-05-06 08:54:11 -0300

    refactor: update data types 📦

--- components/BookedCard/index.tsx
+++ components/BookedCard/index.tsx
@@ -9,10 +9,12 @@ type Props = {
   book: Books
 }
 const BookedCard: React.FC<Props> = ({ book }) => {
-  const { profile } = useSelector((state: AuthState) => state.auth.loggedUser)
+  const { profile, id } = useSelector(
+    (state: AuthState) => state.auth.loggedUser
+  )
 
   const value = book.accepted
-  const isAdmin = profile !== "paciente"
+  const isAdmin = profile !== 'paciente' && book.user.id !== id
 
   if (isAdmin) {
     return <CardAdmin book={book} />
@@ -24,7 +26,7 @@ const BookedCard: React.FC<Props> = ({ book }) => {
         <h2>Pedido de consulta enviado</h2>
       </Row>
       <Row>
-        <p className='text'>
+        <p className="text">
           Obrigado!! Seu pedido de consulta foi recebido.Acompanhe por aqui o
           status do seu pedido. Agora só falta a confirmação do seu pedido pelo
           especialista escolhido.
@@ -38,7 +40,13 @@ const BookedCard: React.FC<Props> = ({ book }) => {
         </Col>
         <Col lg={6}>
           <S.StatusDisplay accepted={value as any}>
-            <p>{value === "pending" ? "PENDENTE" : value === "accepted" ? "ACEITO" : "RECUSADO"}</p>
+            <p>
+              {value === 'pending'
+                ? 'PENDENTE'
+                : value === 'agreed'
+                ? 'ACEITO'
+                : 'RECUSADO'}
+            </p>
           </S.StatusDisplay>
         </Col>
       </Row>
--- components/CardAdmin/index.tsx
+++ components/CardAdmin/index.tsx
@@ -1,9 +1,12 @@
 import AdminModal from 'components/shared/AdminConfirmationModal'
+import AuthState from 'dtos/AuthState'
 import Books from 'dtos/Books'
 import { useState } from 'react'
 import { Col, Row } from 'react-bootstrap'
+import { useSelector } from 'react-redux'
+import { toast } from 'react-toastify'
+import BookedService from 'services/booked'
 import * as S from './styles'
-
 
 type Props = {
   book: Books
@@ -13,44 +16,111 @@ const CardAdmin: React.FC<Props> = ({ book }) => {
   const [show, setShow] = useState(false)
   const [accepted, setAccepted] = useState(true)
 
+  const { profile } = useSelector((state: AuthState) => state.auth.loggedUser)
+  const isAdmin = profile === 'admin'
+  const isEspecialist = profile === 'especialista'
 
-  const value = book.accepted
+  const date = new Date(book.updated_at)
+  const day = date.getDay()
+  const months = [
+    'Jan',
+    'Fev',
+    'Mar',
+    'Abr',
+    'Mai',
+    'Jun',
+    'Jul',
+    'Ago',
+    'Set',
+    'Out',
+    'Nov',
+    'Dez'
+  ]
+  const month = months[date.getMonth()]
+  const year = date.getFullYear()
 
-  const handleOnClose = () => {
+  const handleOnClose = (success?: boolean | 'close') => {
     setShow(false)
+
+    if (success === 'close') return
+    try {
+      isAdmin &&
+        BookedService.update_admin({
+          id: book.id,
+          accepted: success ? 1 : 2
+        })
+      isEspecialist &&
+        BookedService.update_especialist({
+          id: book.id,
+          accepted: success ? 'agreed' : 'rejected'
+        })
+    } catch (error) {
+      console.log(error)
+      toast.error('Ops...algo deu errado ao aceitar/recusar o pedido.')
+    }
   }
 
   return (
     <S.CardWrapper>
-     <AdminModal type={accepted} show={show} handleClose={handleOnClose} target={book.user.name} />
+      <AdminModal
+        type={accepted}
+        show={show}
+        handleClose={success => handleOnClose(success)}
+        target={book.user.name}
+      />
       <Row className="main-title">
-        <i className="fa fa-check-circle" style={{ color: 'green' }}></i>
-        <h2>Pedido recebido</h2>
+        <i
+          className="fa fa-check-circle"
+          style={{ color: book.accepted !== 'pending' ? 'blue' : 'green' }}
+        ></i>
+        <h2>Pedido {book.accepted !== 'pending' ? 'concluído' : 'recebido'}</h2>
       </Row>
       <Row>
-        <p className='text'>
-          Parabéns! Você recebeu um pedido de aluguel para uma de suas salas. O
-          aluguel é referente a {book.room.name}. O possível locatário é {book.user.name}. Para
-          aceitar ou recusar o pedido, dê um clique nos botões abaixo.
-
-        </p>
+        {book.accepted !== 'pending' ? (
+          <p className="text">
+            O pedido de aluguel para uma de suas salas foi{' '}
+            {book.accepted === 'agreed' ? 'aceito' : 'recusado'}. O aluguel é
+            referente a {book.room.name}. O locatário é &nbsp;
+            {book.user.name}.
+          </p>
+        ) : (
+          <p className="text">
+            Parabéns! Você recebeu um pedido de aluguel para uma de suas salas.
+            O aluguel é referente a {book.room.name}. O possível locatário é
+            &nbsp;
+            {book.user.name}. Para aceitar ou recusar o pedido, dê um clique nos
+            botões abaixo.
+          </p>
+        )}
       </Row>
       <Row lg={12}>
         <Col lg={4}>
-          <S.RejectButton onClick={() => {
-            setAccepted(false)
-            setShow(true)
-          }}>
-            <h5>Recusar</h5>
-          </S.RejectButton>
+          {book.accepted !== 'pending' ? (
+            <h5 className="dates">Concluído:</h5>
+          ) : (
+            <S.RejectButton
+              onClick={() => {
+                setAccepted(false)
+                setShow(true)
+              }}
+            >
+              <h5>Recusar</h5>
+            </S.RejectButton>
+          )}
         </Col>
         <Col lg={8}>
-          <S.AcceptedButton onClick={() => {
-            setAccepted(true)
-            setShow(true)
-            }}>
-            <p>Aceitar</p>
-          </S.AcceptedButton>
+          {book.accepted !== 'pending' ? (
+            <p className="dates">{day + ' de ' + month + ' de ' + year}</p>
+          ) : (
+            <S.AcceptedButton
+              onClick={() => {
+                setAccepted(true)
+                setShow(true)
+              }}
+            >
+              <p>Aceitar</p>
+            </S.AcceptedButton>
+          )}
         </Col>
       </Row>
     </S.CardWrapper>
